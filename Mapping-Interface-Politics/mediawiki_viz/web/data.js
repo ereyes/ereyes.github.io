552a0dd37758417da55bc260d9e27a5368d22ce1
Everardo Reyes-Garcia <eve.reyes@gmail.com>
2016-04-08 16:16:54 +0200

    v.0.1

---
+++ Mapping-Interface-Politics/mediawiki_viz/web/data.js
@@ -0,0 +1,811 @@
+var screenshot = [    {
+      "index" : 1,
+      "src" : "001.png",
+      "date" : "June 17",
+      "year" : 2004
+    },
+    {
+      "index" : 2,
+      "src" : "002.png",
+      "date" : "August 15",
+      "year" : 2004
+    },
+    {
+      "index" : 3,
+      "src" : "003.png",
+      "date" : "October 15",
+      "year" : 2004
+    },
+    {
+      "index" : 4,
+      "src" : "004.png",
+      "date" : "November 17",
+      "year" : 2004
+    },
+    {
+      "index" : 5,
+      "src" : "005.png",
+      "date" : "January 26",
+      "year" : 2005
+    },
+    {
+      "index" : 6,
+      "src" : "006.png",
+      "date" : "Februrary 17",
+      "year" : 2005
+    },
+    {
+      "index" : 7,
+      "src" : "007.png",
+      "date" : "March 26",
+      "year" : 2005
+    },
+    {
+      "index" : 8,
+      "src" : "008.png",
+      "date" : "May 25",
+      "year" : 2005
+    },
+    {
+      "index" : 9,
+      "src" : "009.png",
+      "date" : "June 28",
+      "year" : 2005
+    },
+    {
+      "index" : 10,
+      "src" : "010.png",
+      "date" : "July 28",
+      "year" : 2005
+    },
+    {
+      "index" : 11,
+      "src" : "011.png",
+      "date" : "August 28",
+      "year" : 2005
+    },
+    {
+      "index" : 12,
+      "src" : "012.png",
+      "date" : "September 09",
+      "year" : 2005
+    },
+    {
+      "index" : 13,
+      "src" : "013.png",
+      "date" : "December 24",
+      "year" : 2005
+    },
+    {
+      "index" : 14,
+      "src" : "014.png",
+      "date" : "January 29",
+      "year" : 2006
+    },
+    {
+      "index" : 15,
+      "src" : "015.png",
+      "date" : "Februrary 08",
+      "year" : 2006
+    },
+    {
+      "index" : 16,
+      "src" : "016.png",
+      "date" : "Februrary 25",
+      "year" : 2006
+    },
+    {
+      "index" : 17,
+      "src" : "017.png",
+      "date" : "May 15",
+      "year" : 2006
+    },
+    {
+      "index" : 18,
+      "src" : "018.png",
+      "date" : "July 21",
+      "year" : 2006
+    },
+    {
+      "index" : 19,
+      "src" : "019.png",
+      "date" : "August 28",
+      "year" : 2006
+    },
+    {
+      "index" : 20,
+      "src" : "020.png",
+      "date" : "August 30",
+      "year" : 2006
+    },
+    {
+      "index" : 21,
+      "src" : "021.png",
+      "date" : "November 07",
+      "year" : 2006
+    },
+    {
+      "index" : 22,
+      "src" : "022.png",
+      "date" : "November 20",
+      "year" : 2006
+    },
+    {
+      "index" : 23,
+      "src" : "023.png",
+      "date" : "December 27",
+      "year" : 2006
+    },
+    {
+      "index" : 24,
+      "src" : "024.png",
+      "date" : "February 09",
+      "year" : 2007
+    },
+    {
+      "index" : 25,
+      "src" : "025.png",
+      "date" : "June 16",
+      "year" : 2007
+    },
+    {
+      "index" : 26,
+      "src" : "026.png",
+      "date" : "June 16",
+      "year" : 2007
+    },
+    {
+      "index" : 27,
+      "src" : "027.png",
+      "date" : "August 10",
+      "year" : 2007
+    },
+    {
+      "index" : 28,
+      "src" : "028.png",
+      "date" : "October 24",
+      "year" : 2007
+    },
+    {
+      "index" : 29,
+      "src" : "029.png",
+      "date" : "November 29",
+      "year" : 2007
+    },
+    {
+      "index" : 30,
+      "src" : "030.png",
+      "date" : "December 17",
+      "year" : 2007
+    },
+    {
+      "index" : 31,
+      "src" : "031.png",
+      "date" : "January 28",
+      "year" : 2008
+    },
+    {
+      "index" : 32,
+      "src" : "032.png",
+      "date" : "February 03",
+      "year" : 2008
+    },
+    {
+      "index" : 33,
+      "src" : "033.png",
+      "date" : "March 14",
+      "year" : 2008
+    },
+    {
+      "index" : 34,
+      "src" : "034.png",
+      "date" : "October 29",
+      "year" : 2008
+    },
+    {
+      "index" : 35,
+      "src" : "035.png",
+      "date" : "December 17",
+      "year" : 2008
+    },
+    {
+      "index" : 36,
+      "src" : "036.png",
+      "date" : "February 02",
+      "year" : 2009
+    },
+    {
+      "index" : 37,
+      "src" : "037.png",
+      "date" : "March 04",
+      "year" : 2009
+    },
+    {
+      "index" : 38,
+      "src" : "038.png",
+      "date" : "April 17",
+      "year" : 2009
+    },
+    {
+      "index" : 39,
+      "src" : "039.png",
+      "date" : "April 23",
+      "year" : 2009
+    },
+    {
+      "index" : 40,
+      "src" : "040.png",
+      "date" : "June 02",
+      "year" : 2009
+    },
+    {
+      "index" : 41,
+      "src" : "041.png",
+      "date" : "January 22",
+      "year" : 2010
+    },
+    {
+      "index" : 42,
+      "src" : "042.png",
+      "date" : "January 25",
+      "year" : 2010
+    },
+    {
+      "index" : 43,
+      "src" : "043.png",
+      "date" : "February 08",
+      "year" : 2010
+    },
+    {
+      "index" : 44,
+      "src" : "044.png",
+      "date" : "February 24",
+      "year" : 2010
+    },
+    {
+      "index" : 45,
+      "src" : "045.png",
+      "date" : "March 12",
+      "year" : 2010
+    },
+    {
+      "index" : 46,
+      "src" : "046.png",
+      "date" : "April 07",
+      "year" : 2010
+    },
+    {
+      "index" : 47,
+      "src" : "047.png",
+      "date" : "August 07",
+      "year" : 2010
+    },
+    {
+      "index" : 48,
+      "src" : "048.png",
+      "date" : "October 26",
+      "year" : 2010
+    },
+    {
+      "index" : 49,
+      "src" : "049.png",
+      "date" : "November 16",
+      "year" : 2010
+    },
+    {
+      "index" : 50,
+      "src" : "050.png",
+      "date" : "November 18",
+      "year" : 2010
+    },
+    {
+      "index" : 51,
+      "src" : "051.png",
+      "date" : "February 01",
+      "year" : 2011
+    },
+    {
+      "index" : 52,
+      "src" : "052.png",
+      "date" : "February 26",
+      "year" : 2011
+    },
+    {
+      "index" : 53,
+      "src" : "053.png",
+      "date" : "March 09",
+      "year" : 2011
+    },
+    {
+      "index" : 54,
+      "src" : "054.png",
+      "date" : "March 30",
+      "year" : 2011
+    },
+    {
+      "index" : 55,
+      "src" : "055.png",
+      "date" : "April 23",
+      "year" : 2011
+    },
+    {
+      "index" : 56,
+      "src" : "056.png",
+      "date" : "May 05",
+      "year" : 2011
+    },
+    {
+      "index" : 57,
+      "src" : "057.png",
+      "date" : "June 09",
+      "year" : 2011
+    },
+    {
+      "index" : 58,
+      "src" : "058.png",
+      "date" : "June 15",
+      "year" : 2011
+    },
+    {
+      "index" : 59,
+      "src" : "059.png",
+      "date" : "September 11",
+      "year" : 2011
+    },
+    {
+      "index" : 60,
+      "src" : "060.png",
+      "date" : "September 24",
+      "year" : 2011
+    },
+    {
+      "index" : 61,
+      "src" : "061.png",
+      "date" : "November 05",
+      "year" : 2011
+    },
+    {
+      "index" : 62,
+      "src" : "062.png",
+      "date" : "November 30",
+      "year" : 2011
+    },
+    {
+      "index" : 63,
+      "src" : "063.png",
+      "date" : "January 25",
+      "year" : 2012
+    },
+    {
+      "index" : 64,
+      "src" : "064.png",
+      "date" : "February 21",
+      "year" : 2012
+    },
+    {
+      "index" : 65,
+      "src" : "065.png",
+      "date" : "April 05",
+      "year" : 2012
+    },
+    {
+      "index" : 66,
+      "src" : "066.png",
+      "date" : "April 25",
+      "year" : 2012
+    },
+    {
+      "index" : 67,
+      "src" : "067.png",
+      "date" : "May 04",
+      "year" : 2012
+    },
+    {
+      "index" : 68,
+      "src" : "068.png",
+      "date" : "May 25",
+      "year" : 2012
+    },
+    {
+      "index" : 69,
+      "src" : "069.png",
+      "date" : "June 06",
+      "year" : 2012
+    },
+    {
+      "index" : 70,
+      "src" : "070.png",
+      "date" : "June 20",
+      "year" : 2012
+    },
+    {
+      "index" : 71,
+      "src" : "071.png",
+      "date" : "July 04",
+      "year" : 2012
+    },
+    {
+      "index" : 72,
+      "src" : "072.png",
+      "date" : "June 20",
+      "year" : 2012
+    },
+    {
+      "index" : 73,
+      "src" : "073.png",
+      "date" : "August 11",
+      "year" : 2012
+    },
+    {
+      "index" : 74,
+      "src" : "074.png",
+      "date" : "August 29",
+      "year" : 2012
+    },
+    {
+      "index" : 75,
+      "src" : "075.png",
+      "date" : "September 11",
+      "year" : 2012
+    },
+    {
+      "index" : 76,
+      "src" : "076.png",
+      "date" : "September 28",
+      "year" : 2012
+    },
+    {
+      "index" : 77,
+      "src" : "077.png",
+      "date" : "November 14",
+      "year" : 2012
+    },
+    {
+      "index" : 78,
+      "src" : "078.png",
+      "date" : "November 28",
+      "year" : 2012
+    },
+    {
+      "index" : 79,
+      "src" : "079.png",
+      "date" : "January 30",
+      "year" : 2013
+    },
+    {
+      "index" : 80,
+      "src" : "080.png",
+      "date" : "February 04",
+      "year" : 2013
+    },
+    {
+      "index" : 81,
+      "src" : "081.png",
+      "date" : "February 13",
+      "year" : 2013
+    },
+    {
+      "index" : 82,
+      "src" : "082.png",
+      "date" : "March 12",
+      "year" : 2013
+    },
+    {
+      "index" : 83,
+      "src" : "083.png",
+      "date" : "April 25",
+      "year" : 2013
+    },
+    {
+      "index" : 84,
+      "src" : "084.png",
+      "date" : "May 31",
+      "year" : 2013
+    },
+    {
+      "index" : 85,
+      "src" : "085.png",
+      "date" : "June 13",
+      "year" : 2013
+    },
+    {
+      "index" : 86,
+      "src" : "086.png",
+      "date" : "July 04",
+      "year" : 2013
+    },
+    {
+      "index" : 87,
+      "src" : "087.png",
+      "date" : "August 14",
+      "year" : 2013
+    },
+    {
+      "index" : 88,
+      "src" : "088.png",
+      "date" : "October 10",
+      "year" : 2013
+    },
+    {
+      "index" : 89,
+      "src" : "089.png",
+      "date" : "November 05",
+      "year" : 2013
+    },
+    {
+      "index" : 90,
+      "src" : "090.png",
+      "date" : "January 22",
+      "year" : 2014
+    },
+    {
+      "index" : 91,
+      "src" : "091.png",
+      "date" : "February 17",
+      "year" : 2014
+    },
+    {
+      "index" : 92,
+      "src" : "092.png",
+      "date" : "February 28",
+      "year" : 2014
+    },
+    {
+      "index" : 93,
+      "src" : "093.png",
+      "date" : "March 03",
+      "year" : 2014
+    },
+    {
+      "index" : 94,
+      "src" : "094.png",
+      "date" : "April 22",
+      "year" : 2014
+    },
+    {
+      "index" : 95,
+      "src" : "095.png",
+      "date" : "May 18",
+      "year" : 2014
+    },
+    {
+      "index" : 96,
+      "src" : "096.png",
+      "date" : "July 23",
+      "year" : 2014
+    },
+    {
+      "index" : 97,
+      "src" : "097.png",
+      "date" : "August 21",
+      "year" : 2014
+    },
+    {
+      "index" : 98,
+      "src" : "098.png",
+      "date" : "September 24",
+      "year" : 2014
+    },
+    {
+      "index" : 99,
+      "src" : "099.png",
+      "date" : "September 30",
+      "year" : 2014
+    },
+    {
+      "index" : 100,
+      "src" : "100.png",
+      "date" : "October 10",
+      "year" : 2014
+    },
+    {
+      "index" : 101,
+      "src" : "101.png",
+      "date" : "October 24",
+      "year" : 2014
+    },
+    {
+      "index" : 102,
+      "src" : "102.png",
+      "date" : "November 19",
+      "year" : 2014
+    },
+    {
+      "index" : 103,
+      "src" : "103.png",
+      "date" : "November 24",
+      "year" : 2014
+    },
+    {
+      "index" : 104,
+      "src" : "104.png",
+      "date" : "December 06",
+      "year" : 2014
+    },
+    {
+      "index" : 105,
+      "src" : "105.png",
+      "date" : "December 17",
+      "year" : 2014
+    },
+    {
+      "index" : 106,
+      "src" : "106.png",
+      "date" : "December 28",
+      "year" : 2014
+    },
+    {
+      "index" : 107,
+      "src" : "107.png",
+      "date" : "January 07",
+      "year" : 2015
+    },
+    {
+      "index" : 108,
+      "src" : "108.png",
+      "date" : "January 23",
+      "year" : 2015
+    },
+    {
+      "index" : 109,
+      "src" : "109.png",
+      "date" : "February 11",
+      "year" : 2015
+    },
+    {
+      "index" : 110,
+      "src" : "110.png",
+      "date" : "February 28",
+      "year" : 2015
+    },
+    {
+      "index" : 111,
+      "src" : "111.png",
+      "date" : "March 29",
+      "year" : 2015
+    },
+    {
+      "index" : 112,
+      "src" : "112.png",
+      "date" : "April 12",
+      "year" : 2015
+    },
+    {
+      "index" : 113,
+      "src" : "113.png",
+      "date" : "May 06",
+      "year" : 2015
+    },
+    {
+      "index" : 114,
+      "src" : "114.png",
+      "date" : "May 28",
+      "year" : 2015
+    },
+    {
+      "index" : 115,
+      "src" : "115.png",
+      "date" : "June 21",
+      "year" : 2015
+    },
+    {
+      "index" : 116,
+      "src" : "116.png",
+      "date" : "June 21",
+      "year" : 2015
+    },
+    {
+      "index" : 117,
+      "src" : "117.png",
+      "date" : "July 21",
+      "year" : 2015
+    },
+    {
+      "index" : 118,
+      "src" : "118.png",
+      "date" : "August 21",
+      "year" : 2015
+    },
+    {
+      "index" : 119,
+      "src" : "119.png",
+      "date" : "August 28",
+      "year" : 2015
+    },
+    {
+      "index" : 120,
+      "src" : "120.png",
+      "date" : "September 11",
+      "year" : 2015
+    },
+    {
+      "index" : 121,
+      "src" : "121.png",
+      "date" : "October 09",
+      "year" : 2015
+    },
+    {
+      "index" : 122,
+      "src" : "122.png",
+      "date" : "November 04",
+      "year" : 2015
+    },
+    {
+      "index" : 123,
+      "src" : "123.png",
+      "date" : "November 04",
+      "year" : 2015
+    },
+    {
+      "index" : 124,
+      "src" : "124.png",
+      "date" : "November 06",
+      "year" : 2015
+    },
+    {
+      "index" : 125,
+      "src" : "125.png",
+      "date" : "November 06",
+      "year" : 2015
+    },
+    {
+      "index" : 126,
+      "src" : "126.png",
+      "date" : "November 28",
+      "year" : 2015
+    },
+    {
+      "index" : 127,
+      "src" : "127.png",
+      "date" : "December 11",
+      "year" : 2015
+    },
+    {
+      "index" : 128,
+      "src" : "128.png",
+      "date" : "December 29",
+      "year" : 2015
+    },
+    {
+      "index" : 129,
+      "src" : "129.png",
+      "date" : "January 13",
+      "year" : 2016
+    },
+    {
+      "index" : 130,
+      "src" : "130.png",
+      "date" : "January 19",
+      "year" : 2016
+    },
+    {
+      "index" : 131,
+      "src" : "131.png",
+      "date" : "February 22",
+      "year" : 2016
+    },
+    {
+      "index" : 132,
+      "src" : "132.png",
+      "date" : "February 28",
+      "year" : 2016
+    },
+    {
+      "index" : 133,
+      "src" : "133.png",
+      "date" : "March 06",
+      "year" : 2016
+    },
+    {
+      "index" : 134,
+      "src" : "134.png",
+      "date" : "March 11",
+      "year" : 2016
+    },
+    {
+      "index" : 135,
+      "src" : "135.png",
+      "date" : "March 11",
+      "year" : 2016
+    }
+  ];
